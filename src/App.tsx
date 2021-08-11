945c244b2b8ecd39806186b9cfe6e859b256a63c
Chaim <calvinfriedman@calvins-mbp-2.lan>
2021-08-10 23:30:12 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+`;
+
+const Left = styled.div`
+  width: 25%;
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
 function App() {
+  const [url, setUrl] = useState('');
+  const [urlToDisplay, setUrlToDisplay] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Left>
+        <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+        <button onClick={() => setUrlToDisplay(url)}>Set</button>
+      </Left>
+
+      <Right>
+        <iframe title="iframe" style={{ height: '100%', width: '100%' }} src={urlToDisplay} />
+      </Right>
+
+    </Container>
   );
 }
 
